99d6dd5f5214fcc88cb5d4e2a6e6454d849799e8
Vicky Gupta <vickyguptaa7@gmail.com>
2023-06-15 12:59:19 +0530

    about us done

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,16 +4,16 @@ import Wrapper from "../Wrapper/wrapper";
 
 function About() {
   return (
-    <section className="mt-24 ">
+    <section id="about" className="mt-24">
       <div className="flex flex-col items-center justify-center px-8 header">
         <h1 className="text-[color:var(--color-primary)] text-3xl font-bold mb-2 text-center">
           ABOUT ME
         </h1>
         <div className="h-[0.30rem] w-10 bg-[color:var(--color-primary)] rounded-full"></div>
       </div>
-      <div className="w-full py-4 bg-[color:var(--color-primary)] mt-20 relative">
-        <Wrapper>
-          <div className="flex flex-col items-center justify-center gap-4 sm:flex-row sm:gap-12">
+      <div className="w-full py-12 bg-[color:var(--color-primary)] mt-24 relative">
+        <Wrapper className="flex flex-col items-center justify-center gap-8">
+          <div className="relative flex flex-col items-center justify-center gap-4 sm:flex-row sm:gap-12">
             <motion.div
               className="w-48 bg-white border-8 border-white rounded-full sm:w-64 aspect-square"
               initial={{
@@ -34,30 +34,43 @@ function About() {
                 className="flex items-center justify-center object-cover w-full h-full rounded-full"
               />
             </motion.div>
-            <div className="flex flex-col items-center justify-center gap-4 tracking-wide text-white text-md w-[60%]">
-              <p>
+            <div className="flex flex-col items-center justify-center gap-4 tracking-wide text-white text-md sm:w-[60%]">
+              <p className="text-[1.1rem]">
                 Hi, I'm Vicky Gupta. I'm a self-taught, passionate full-stack
                 web developer from Delhi, India. I'm currently a B.Tech student
                 in Computer Engineering at Jamia Millia Islamia.
               </p>
-              <p>
+              <p className="text-[1.1rem]">
                 I enjoy the challenges of competitive programming, which has
-                sharpened my critical thinking and coding skills. I have a max
-                rating of 1600 on Codeforces and 2217 on Leetcode. These
+                sharpened my critical thinking and coding skills. These
                 experiences have enabled me to tackle complex tasks with
                 confidence.
               </p>
+            </div>
+          <div className="left-0 flex flex-col items-center justify-between w-full gap-8 mt-6 sm:items-start sm:flex-row sm:absolute -bottom-48">
+            <div className="p-4 text-[color:var(--primary-text-color)] bg-white rounded-lg shadow-lg shadow-gray-300 flex flex-col items-end gap-4 max-w-[20rem]">
+              <p>“Simplicity is the baddest choice to be the best.”</p>
+              <h4 className="text-sm font-light">- vicky gupta</h4>
+            </div>
+            <div className="p-4 mt-4 text-[color:var(--primary-text-color)] bg-white rounded-lg shadow-lg  shadow-gray-300 flex flex-col items-end gap-4 max-w-[20rem]">
               <p>
-                In 2022, my team and I were fortunate to be finalists in the
-                Smart India Hackathon (SIH). We collaborated with talented
-                individuals from diverse backgrounds to develop innovative
-                solutions that showcased our creativity and technical abilities.
+                “I know I'm not successful enough, but I’m passionate enough not
+                to worry about success.”
               </p>
+              <h4 className="text-sm font-light">- vicky gupta</h4>
+            </div>
+            <div className="absolute sm:static -bottom-40 p-4 mt-8 text-[color:var(--primary-text-color)] bg-white rounded-lg shadow-lg  shadow-gray-300 flex flex-col items-end gap-4 max-w-[20rem]">
+              <p>
+                “Creativity is the driver of an unstoppable train called
+                Passion.”
+              </p>
+              <h4 className="text-sm font-light">- vicky gupta</h4>
             </div>
           </div>
+          </div>
         </Wrapper>
-        <div className="absolute -top-16 w-[110%] -z-10 -translate-x-5 h-24 bg-[--color-primary] rotate-3 mt-6"></div>
-        <div className="absolute -bottom-12 w-[110%] -z-10 -translate-x-5 h-24 bg-[--color-primary] rotate-3"></div>
+        <div className="absolute -top-20 w-[110%] -z-10 -translate-x-5 h-56 bg-[--color-primary] rotate-3 mt-6"></div>
+        <div className="absolute -bottom-16 w-[110%] -z-10 -translate-x-5 h-56 bg-[--color-primary] rotate-3"></div>
       </div>
     </section>
   );
